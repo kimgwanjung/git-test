3fedb71661af5c3f49e5652d750c84beb6e54360
kimgwanjung <qwe2846@naver.com>
2023-03-05 20:49:34 +0900

    git test

---
+++ starpick/src/starpick/halfTriangle.java
@@ -0,0 +1,13 @@
+package starpick;
+
+public class halfTriangle {
+	public static void main(String[] args){
+		for(int i=1; i<5;i++) {
+			for(int j=0;j<i;j++) {
+				System.out.print("*");	
+			}
+			System.out.println("");
+		}	
+	}
+
+}
